835ab7ba3a5c89071bccafec8c14dfb542aeb516
Yujung Dong <33361993+dongyujung@users.noreply.github.com>
2018-05-24 16:52:10 -0700

    Update yes_or_no.py

--- yes_or_no.py
+++ yes_or_no.py
@@ -1,4 +1,9 @@
 def yes_or_no(question):
+    """
+    Returns True for input starting with y or N
+    Returns False for input starting with n or N
+    Repeat question when neither y nor n was input
+    """
     reply = str(raw_input(question+' (y/n): ')).lower()
     if reply[0] == 'y':
         return True
