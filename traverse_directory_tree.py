22fc2e0ac6a45d7c1ca37df58220688b01190fb6
Yujung Dong <33361993+dongyujung@users.noreply.github.com>
2018-06-04 00:53:59 -0700

    Update traverse_directory_tree.py

--- traverse_directory_tree.py
+++ traverse_directory_tree.py
@@ -2,6 +2,7 @@ from os import walk
 
 rootDir = 'C:\Users\Documents\Yujung'
 
+# Traversing directory tree with os.walk()
 for dirName, subdirList, fileList in os.walk(rootDir):
     print 'Found directory: %s' % dirName
     for fname in fileList:
