a0b21fb3531ddfbfe796b19cfb885911b3c23eab
Yujung Dong <33361993+dongyujung@users.noreply.github.com>
2018-06-04 00:59:24 -0700

    Update find_matching_file.py

--- find_matching_file.py
+++ find_matching_file.py
@@ -3,8 +3,8 @@ import re
 
 def find_matching_file(pattern, path):
     """
-    Find file within the path that starts with pattern
-    using re.match
+    Find file within the path that starts with pattern using re.match
+    and return the file path including its name
     """
     for dir_name, subdir_list, file_list in os.walk(path):
         for each_file in file_list:
